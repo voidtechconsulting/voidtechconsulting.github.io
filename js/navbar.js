03506f787d000a74dc4962a5c733929fba66559f
Lizapritchardg <lizap@umich.edu>
2021-08-26 14:49:06 -0400

    add mobile navbar

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector('.menuToggle');
+const navMenu = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-link');
+
+hamburger.addEventListener('click', menuToggleClick);
+
+function menuToggleClick() {
+  hamburger.classList.toggle("menu-open");
+  navMenu.classList.toggle("menu-open");
+}
