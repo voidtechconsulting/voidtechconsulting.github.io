cd91b0e8160ec2e90ba08545eec69e39e044cfe1
Lizapritchardg <lizap@umich.edu>
2021-08-27 12:52:42 -0400

    update mobile navbar to close when section is selected

--- js/navbar.js
+++ js/navbar.js
@@ -3,8 +3,10 @@ const navMenu = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-link');
 
 hamburger.addEventListener('click', menuToggleClick);
+navLinks.forEach(navLink => navLink.addEventListener('click', menuToggleClick));
 
 function menuToggleClick() {
   hamburger.classList.toggle("menu-open");
   navMenu.classList.toggle("menu-open");
 }
+
